dadf2cae7030bb0ff5527369fe6317e0989f7bbf
yusungkk <roawnd5@ajou.ac.kr>
2025-01-22 16:45:40 +0900

    Update TestController.java

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    return "Hello Elice ### 1";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
